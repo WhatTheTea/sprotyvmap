7a23dec40bec8a5abefb9de830d0cac951a1b2c0
WhatTheTea <silchenko.whatthetea@gmail.com>
2023-11-03 11:57:44 +0200

    visicom secret and github workflow yeet

--- functions/main.py
+++ functions/main.py
@@ -2,10 +2,15 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 from firebase_functions import https_fn
+from firebase_functions.params import SecretParam
+
 from firebase_admin import initialize_app
+
 from sprotyvmap_api import sm_api
 
 initialize_app()
+VISICOM_API_KEY = SecretParam('VISICOM')
+sm_api.set_visicom_key(VISICOM_API_KEY.value)
 
 @https_fn.on_request()
 def flask_request(req: https_fn.Request) -> https_fn.Response:
