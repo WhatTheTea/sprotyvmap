ee7b32a83f12db3f5c090c349b249aa9c4cfcc6e
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-05-05 22:43:07 +0300

    Add SprotyvInUaScraperTests

---
+++ tests/WebScraper.Tests/SprotyvInUaScraperTest.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using WhatTheTea.SprotyvMap.WebScraper;
+using Xunit;
+
+namespace WhatTheTea.SprotyvMap.WebScraper.Tests;
+
+[TestSubject(typeof(SprotyvInUaScraper))]
+public class SprotyvInUaScraperTest
+{
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}
