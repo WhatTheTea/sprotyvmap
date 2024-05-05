7e3ee6bfa8fd9ebe17f77ac8f70d3cc000f1d6ed
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-05-05 23:52:12 +0300

    Add SprotyvInUaScraperTest

--- tests/WebScraper.Tests/SprotyvInUaScraperTest.cs
+++ tests/WebScraper.Tests/SprotyvInUaScraperTest.cs
@@ -1,4 +1,10 @@
-﻿using JetBrains.Annotations;
+﻿using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using JetBrains.Annotations;
+using RichardSzalay.MockHttp;
 using WhatTheTea.SprotyvMap.WebScraper;
 using Xunit;
 
@@ -7,10 +13,32 @@ namespace WhatTheTea.SprotyvMap.WebScraper.Tests;
 [TestSubject(typeof(SprotyvInUaScraper))]
 public class SprotyvInUaScraperTest
 {
+    private const string SiteUri = "https://sprotyv.in.ua/";
+    private const string TestSiteUri = "TestData/sprotyv.in.ua.htm";
+
+    private SprotyvInUaScraper Scraper { get; set; }
+    
+    public SprotyvInUaScraperTest()
+    {
+        var httpMock = new MockHttpMessageHandler();
+        var testPage = File.ReadAllText(TestSiteUri);
+        httpMock.When(SiteUri)
+            .Respond("text/html", testPage);
+        var httpClient = httpMock.ToHttpClient();
+
+        this.Scraper = SprotyvInUaScraper.CreateAsync(httpClient)
+            .GetAwaiter()
+            .GetResult();
+    }
 
     [Fact]
-    public void METHOD()
+    public async Task FirstEntryIsVinnytsky()
     {
-        
+        var result = await Scraper.GetAllDistrictsAsync();
+        var district = result.First();
+        var centre = district.EquipmentCentres.First();
+
+        district.Name.Should().Be("Вінницька область");
+        centre.Title.Should().Be("Вінницький ОТЦК та СП");
     }
 }
