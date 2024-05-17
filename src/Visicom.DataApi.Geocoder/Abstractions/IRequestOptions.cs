8d6f18f6cef65de48436481b62ecc27fcfa4dc87
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-05-18 01:49:07 +0300

    Change request options to use new enums

--- src/Visicom.DataApi.Geocoder/Abstractions/IRequestOptions.cs
+++ src/Visicom.DataApi.Geocoder/Abstractions/IRequestOptions.cs
@@ -1,20 +1,28 @@
+using Visicom.DataApi.Geocoder.Enums;
+
 namespace Visicom.DataApi.Geocoder.Abstractions;
 
 public interface IRequestOptions
 {
     public string ApiKey { get; }
-    public string Language { get; }
-    public IEnumerable<string> Categories { get; }
-    public IEnumerable<string> ExcludeCategories { get; }
+    public Languages Language { get; }
+    public IEnumerable<Categories> Categories { get; }
+    public IEnumerable<Categories> ExcludeCategories { get; }
     public string Text { get; }
     public string WordText { get; }
     public string Near { get; }
     public string NearRadius { get; }
-    public string Order { get; }
+    public Order Order { get; }
     public string IntersectionArea { get; }
     public string ContainsArea { get; }
     public string Zoom { get; }
     public string Limit { get; }
+    /// <summary>
+    /// ISO 3166-1 alpha-2
+    /// </summary>
     public string CountryCode { get; }
+    /// <summary>
+    /// ISO 3166-1 alpha-2
+    /// </summary>
     public string CountryCodePriority { get; }
 }
--- src/Visicom.DataApi.Geocoder/RequestOptions.cs
+++ src/Visicom.DataApi.Geocoder/RequestOptions.cs
@@ -1,12 +1,21 @@
 using Visicom.DataApi.Geocoder.Abstractions;
+using Visicom.DataApi.Geocoder.Enums;
 
 namespace Visicom.DataApi.Geocoder;
 
-public class RequestOptions(string language, string apikey) : IRequestOptions
+public class RequestOptions : IRequestOptions
 {
+    private string _apikey = string.Empty;
+
+    public RequestOptions(Languages language, string apikey)
+    {
+        Language = language;
+        ApiKey = apikey;
+    }
+
     public string ApiKey
     {
-        get => apikey;
+        get => _apikey;
         set
         {
             if (string.IsNullOrWhiteSpace(value))
@@ -14,31 +23,18 @@ public class RequestOptions(string language, string apikey) : IRequestOptions
                 throw new ArgumentException("Value was empty");
             }
 
-            apikey = value;
+            _apikey = value;
         }
     }
 
-    public string Language
-    {
-        get => language;
-        set
-        {
-            if (string.IsNullOrWhiteSpace(value))
-            {
-                throw new ArgumentException("Value was empty");
-            }
-
-            language = value;
-        }
-    }
-
-    public IEnumerable<string> Categories { get; set; } = [];
-    public IEnumerable<string> ExcludeCategories { get; set; } = [];
+    public Languages Language { get; set; }
+    public IEnumerable<Categories> Categories { get; set; } = [];
+    public IEnumerable<Categories> ExcludeCategories { get; set; } = [];
     public string Text { get; set; } = string.Empty;
     public string WordText { get; set; } = string.Empty;
     public string Near { get; set; } = string.Empty;
     public string NearRadius { get; set; } = string.Empty;
-    public string Order { get; set; } = string.Empty;
+    public Order Order { get; set; }
     public string IntersectionArea { get; set; } = string.Empty;
     public string ContainsArea { get; set; } = string.Empty;
     public string Zoom { get; set; } = string.Empty;
