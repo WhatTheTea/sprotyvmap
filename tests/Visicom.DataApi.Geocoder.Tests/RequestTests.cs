9579df4ff8bad4f6d84ac514a4fdd56fe08ac1d7
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-05-24 18:41:25 +0300

    Add httpMock to RequestTests.cs

--- tests/Visicom.DataApi.Geocoder.Tests/RequestTests.cs
+++ tests/Visicom.DataApi.Geocoder.Tests/RequestTests.cs
@@ -1,7 +1,12 @@
+using System.IO;
 using System.Net.Http;
+using System.Net.Mime;
 using System.Threading.Tasks;
+using System.Web;
 using FluentAssertions;
+using RichardSzalay.MockHttp;
 using Visicom.DataApi.Geocoder;
+using Visicom.DataApi.Geocoder.Abstractions;
 using Visicom.DataApi.Geocoder.Enums;
 using WhatTheTea.SprotyvMap.Primitives;
 using Xunit;
@@ -10,17 +15,29 @@ namespace Visicom.DataApi.Geocoder.Tests;
 
 public class RequestTests
 {
-    // https://api.visicom.ua/data-api/5.0/uk/geocode.json?text=%D0%BC.%20%D0%9A%D0%B8%D1%97%D0%B2,%20%D0%B2%D1%83%D0%BB.%20%D0%A5%D1%80%D0%B5%D1%89%D0%B0%D1%82%D0%B8%D0%BA,%2026
+    private const string ApiKey = "API_KEY";
+    private const string TestResponsePath = "TestData/IdealResponse.json";
+    private const string KyivRequest = $"https://api.visicom.ua/data-api/5.0/uk/geocode.json?key={ApiKey}&t=м. Київ, вул. Хрещатик, 26&order=relevance";
+    
+    private IGeocoder Geocoder { get; }
+    public RequestTests()
+    {
+        var httpMock = new MockHttpMessageHandler();
+        var testResponse = File.ReadAllText(TestResponsePath);
+        httpMock.When("*")
+            .With(x => HttpUtility.UrlDecode(x.RequestUri?.ToString()) == KyivRequest)
+            .Respond(MediaTypeNames.Application.Json, testResponse);
+        
+        var options = new RequestOptions(Languages.Ukrainian, ApiKey);
+        Geocoder = new BasicGeocoder(httpMock.ToHttpClient(), options);
+    }
+
     [Fact]
     public async Task GetCoordinatesOfKyiv()
     {
-        var options = new RequestOptions(Languages.Ukrainian, "apikey");
-        var httpClient = new HttpClient();
-        var geocoder = new BasicGeocoder(httpClient, options);
-        
-        var result = await geocoder.GetCoordinatesAsync("Kyiv");
+        var result = await Geocoder.GetCoordinatesAsync("м. Київ, вул. Хрещатик, 26");
 
         result.Should()
-            .BeEquivalentTo(new MapPoint(50.4015698,30.2030564));
+            .BeEquivalentTo(new MapPoint(50.448847,30.521626));
     }
 }
