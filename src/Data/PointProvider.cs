a6e304760699392a514f282128cf3e75ce519b9f
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-05-24 21:41:47 +0300

    Add PointProvider.cs

---
+++ src/Data/PointProvider.cs
@@ -0,0 +1,32 @@
+namespace WhatTheTea.SprotyvMap.Data;
+
+public class PointProvider : IPointProvider
+{
+    public EquipmentCentre GetPoint(int district, int id)
+        => GetPointAsync(district, id)
+            .ConfigureAwait(false)
+            .GetAwaiter().GetResult();
+
+    public List<EquipmentCentre> GetPointsByDistrict(int district)
+        => GetPointsByDistrictAsync(district).ConfigureAwait(false)
+            .GetAwaiter().GetResult();
+
+    public List<EquipmentCentre> GetAllPoints()
+        => GetAllPointsAsync().ConfigureAwait(false)
+            .GetAwaiter().GetResult();
+
+    public Task<EquipmentCentre> GetPointAsync(int district, int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<EquipmentCentre>> GetPointsByDistrictAsync(int district)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<EquipmentCentre>> GetAllPointsAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
