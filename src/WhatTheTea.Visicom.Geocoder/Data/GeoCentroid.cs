ec20dae14818c70631b61a2d03d6b7c9ad0003ca
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-05-25 22:29:12 +0300

    Extract visicom geocoder to separate directory

--- src/WhatTheTea.Visicom.Geocoder/Data/GeoCentroid.cs
+++
@@ -1,8 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace WhatTheTea.Visicom.Geocoder.Data;
-
-public record GeoCentroid(
-    [property: JsonPropertyName("type")] string Type,
-    [property: JsonPropertyName("coordinates")] IReadOnlyList<double> Coordinates
-);
