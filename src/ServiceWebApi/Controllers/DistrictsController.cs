ffd55c86f57d6204bfb1c90cd400b3563ace87fb
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-05-30 20:56:52 +0300

    Change service type to web api

---
+++ src/ServiceWebApi/Controllers/DistrictsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhatTheTea.SprotyvMap.Shared.Abstractions;
+using WhatTheTea.SprotyvMap.Shared.Primitives;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WhatTheTea.SportyvMap.EquipmentCentreService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DistrictsController(IDataProvider dataProvider, IMapPointProvider mapPointProvider) : ControllerBase
+    {
+        private readonly IDataProvider dataProvider = dataProvider;
+        private readonly IMapPointProvider mapPointProvider = mapPointProvider;
+
+        // GET: api/<EquipmentCentres>
+        [HttpGet]
+        public async Task<IEnumerable<District>> Get()
+        {
+            var data = (await dataProvider.GetAllDistrictsAsync()).ToList();
+            return await Task.WhenAll(data.Select(async x =>
+            {
+                var centres = x.EquipmentCentres.Select(async y =>
+                {
+                    var mapPoint = await mapPointProvider.GetPoint(y.Information);
+                    return y with { Point = mapPoint };
+                });
+                return x with { EquipmentCentres = await Task.WhenAll(centres) };
+            }));
+        }
+
+        // GET api/EquipmentCentres/district/1/centre/1
+        [HttpGet("district/{district}/centre/{id}")]
+        public async Task<EquipmentCentre> Get(int district, int id)
+        {
+            var data = await dataProvider.GetEquipmentCentreAsync(district, id);
+            var mapPoint = await mapPointProvider.GetPoint(data.Information);
+
+            return data with { Point = mapPoint };
+        }
+    }
+}
---
+++ src/ServiceWebApi/Program.cs
@@ -0,0 +1,38 @@
+using Visicom.DataApi.Geocoder;
+using Visicom.DataApi.Geocoder.Enums;
+using WhatTheTea.SprotyvMap.Service;
+using WhatTheTea.SprotyvMap.Shared.Abstractions;
+using WhatTheTea.SprotyvMap.SprotyvInUa;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<HttpClient>();
+builder.Services.AddControllers();
+
+var visicomApiKey = builder.Configuration["VISICOM_DAPI_KEY"] ?? string.Empty;
+var requestOptions = new RequestOptions(Languages.Ukrainian, visicomApiKey);
+builder.Services
+    .AddScoped<IMapPointProvider, VisicomMapPointProvider>(provider =>
+        new VisicomMapPointProvider(provider.GetService<HttpClient>()!, requestOptions))
+    .AddScoped<IDataProvider, WebScraper>(s => WebScraper.Create(s.GetService<HttpClient>()!).GetAwaiter().GetResult());
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.UseHttpsRedirection();
+
+app.Run();
