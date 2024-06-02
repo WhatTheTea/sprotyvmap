2ef6295b189a51324403e85b331c44bdee009814
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-06-02 14:14:11 +0300

    Add GetDistrictsAsync implementation

--- src/WebApp/Services/ApiEquipmentCentreProvider.cs
+++ src/WebApp/Services/ApiEquipmentCentreProvider.cs
@@ -1,14 +1,19 @@
-﻿using WhatTheTea.SprotyvMap.Shared.Primitives;
+﻿using System.Net.Http.Json;
+
+using WhatTheTea.SprotyvMap.Shared.Primitives;
 
 namespace WhatTheTea.SprotyvMap.WebApp.Services
 {
-    public class ApiEquipmentCentreProvider(HttpClient httpClient) : IEquipmentCentreProvider
+    public class ApiEquipmentCentreProvider(HttpClient httpClient, string apiUrl = "https://sprotyvmap-api.azurewebsites.net") : IEquipmentCentreProvider
     {
         private readonly HttpClient httpClient = httpClient;
+        private readonly string apiUrl = apiUrl;
+        private string apiDistrictsEndpoint => apiUrl + "api/Districts";
 
-        public Task<IEnumerable<District>> GetDistrictsAsync()
+        public async Task<IEnumerable<District>> GetDistrictsAsync()
         {
-            throw new NotImplementedException();
+            var response = await httpClient.GetFromJsonAsync<District[]>(apiDistrictsEndpoint);
+            return response ?? [];
         }
     }
 }
