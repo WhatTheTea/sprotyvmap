bc6538fb13999341a1bca8cf16e60bde6657efff
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-06-03 17:23:22 +0300

    Add caching

---
+++ src/EquipmentCentreService/Extensions/EquipmentCentreExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Visicom.DataApi.Geocoder.Enums;
+using Visicom.DataApi.Geocoder;
+using WhatTheTea.SprotyvMap.Service;
+using WhatTheTea.SprotyvMap.Shared.Abstractions;
+using WhatTheTea.SprotyvMap.SprotyvInUa;
+
+namespace WhatTheTea.SportyvMap.EquipmentCentreService.Extensions;
+
+public static class EquipmentCentresExtensions
+{
+    public static IServiceCollection AddEquipmentCentreDataProviders(this IServiceCollection services, string visicomApiKey) 
+    {
+        var requestOptions = new RequestOptions(Languages.Ukrainian, visicomApiKey);
+
+        services.AddScoped<IMapPointProvider, VisicomMapPointProvider>(services =>
+                new VisicomMapPointProvider(
+                    services.GetRequiredService<HttpClient>(),
+                    requestOptions,
+                    services.GetRequiredService<ILogger<VisicomMapPointProvider>>())
+                );
+
+        services.AddScoped<IDataProvider, WebScraper>(services =>
+            WebScraper.Create(services.GetRequiredService<HttpClient>())
+            .GetAwaiter()
+            .GetResult());
+
+        return services;
+    }
+}
---
+++ src/EquipmentCentreService/Workers/DistritctsCacheWorker.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+using WhatTheTea.SportyvMap.EquipmentCentreService.Services;
+using WhatTheTea.SprotyvMap.Shared.Primitives;
+
+namespace WhatTheTea.SportyvMap.EquipmentCentreService.Workers;
+
+public sealed class DistritctsCacheWorker(
+    IEquipmentCentreService service,
+    CacheSignal<District[]> cacheSignal,
+    IMemoryCache memoryCache,
+    ILogger<DistritctsCacheWorker> logger)
+    : BackgroundService
+{
+    private readonly IEquipmentCentreService service = service;
+    private readonly CacheSignal<District[]> cacheSignal = cacheSignal;
+    private readonly IMemoryCache memoryCache = memoryCache;
+    private readonly ILogger<DistritctsCacheWorker> logger = logger;
+
+    private readonly TimeSpan updateInterval = TimeSpan.FromDays(7);
+    private bool isCacheInitialized = false;
+
+    public override async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await cacheSignal.WaitAsync();
+        await base.StartAsync(cancellationToken);
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                await UpdateCache();
+            }
+            finally
+            {
+                if (!isCacheInitialized)
+                {
+                    cacheSignal.Release();
+                    isCacheInitialized = true;
+                }
+            }
+
+            try
+            {
+                await Task.Delay(updateInterval, stoppingToken);
+            }
+            catch (OperationCanceledException)
+            {
+                logger.LogWarning("Caching was cancelled");
+                break;
+            }
+        }
+    }
+
+    private async Task UpdateCache() 
+    {
+        District[] districts = (await service.GetDistrictsAsync()).ToArray();
+
+        if (districts.Length == 0)
+        {
+            logger.LogError("Unable to update districts cache");
+        } 
+        else
+        {
+            memoryCache.Set("Districts", districts);
+            logger.LogInformation("Cache has been updated");
+        }
+    }
+}
